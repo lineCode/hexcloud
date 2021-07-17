0a0ce90df1c3c42ac189627354485b4a0ea83a93
3vilM33pl3 <ovanac01@mail.bbk.ac.uk>
2021-07-17 12:34:31 +0100

    Fill circle functionality added

--- hexlib/HexagonService.cpp
+++ hexlib/HexagonService.cpp
@@ -4,18 +4,24 @@
 Status HexagonService::GetHexagonRing(::grpc::ServerContext *context, const ::hexagon::HexagonRingRequest *request,
                                       ::hexagon::HexCubeResponse *response) {
     auto hexpb = request->ha();
-    auto result = HexagonLibrary::Ring(Hexagon(hexpb.x(), hexpb.y(), hexpb.z()), request->radius());
-    std::cout << "Request: X: " << hexpb.x() << " Y: " << hexpb.y() << " Z: " << hexpb.z() << std::endl;
-
-    google::protobuf::RepeatedPtrField<Hex> hexpbv;
-    for(auto hex : result) {
-        std::cout << "Response: Q: " << hex.Q << " R: " << hex.R << " S: " << hex.S << std::endl;
-        auto hexpb = response->mutable_hc()->Add();
-        hexpb->set_x(hex.Q);
-        hexpb->set_y(hex.R);
-        hexpb->set_z(hex.S);
+    int maxStep = 1;
+    if(request->fill()) {
+        maxStep = request->radius();
     }
 
+    for(int step = 0; step <= maxStep; step++) {
+        auto result = HexagonLibrary::Ring(Hexagon(hexpb.x(), hexpb.y(), hexpb.z()), request->radius() - step);
+        std::cout << "Request: X: " << hexpb.x() << " Y: " << hexpb.y() << " Z: " << hexpb.z() << std::endl;
+
+        google::protobuf::RepeatedPtrField <Hex> hexpbv;
+        for (auto hex : result) {
+            std::cout << "Response: Q: " << hex.Q << " R: " << hex.R << " S: " << hex.S << std::endl;
+            auto hexpb = response->mutable_hc()->Add();
+            hexpb->set_x(hex.Q);
+            hexpb->set_y(hex.R);
+            hexpb->set_z(hex.S);
+        }
+    }
 
 
     return Status::OK;
